8f929f12199ba48dad4db47c395547d065ef93d8
Stavros Tsourlidakis <s.tsourlidakis@hotmail.com>
2016-10-01 15:46:34 +0300

    implement  /scores API endpoint
    returns all score entries

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,35 @@
 <?php
-	$app->get('/api/v1/test', function () use ($app, $db){
-		echo "It's alive!";
+	/**
+	*	GET ALL SCORES
+	**/
+	$app->get('/api/v1/scores', function () use ($app, $db){
+		$stmt = $db->prepare("SELECT `id`,`username`,`year`,`month`,`gen_score`,`placeholder1`,`placeholder2`,`placeholder3` FROM `raw_scores` WHERE 1");
+		$stmt->execute();
+		
+		$stmt->bind_result($score_id, $score_username, $score_year, $score_month, $score_gen_score, $score_placeholder1, $score_placeholder2, $score_placeholder3);
+		
+		
+		$response = array();
+		$response['scores'] = array();
+		while($stmt->fetch()){
+			$score = array();	//reinit the array just in case
+			
+			$score['id'] = $score_id;
+			$score['username'] = $score_username;
+			$score['year'] = $score_year;
+			$score['month'] = $score_month;
+			$score['gen_score'] = $score_gen_score;
+			$score['placeholder1'] = $score_placeholder1;
+			$score['placeholder2'] = $score_placeholder2;
+			$score['placeholder3'] = $score_placeholder3;
+			
+			$response['scores'][] = $score;
+		}
+		$stmt->close();
+		
+		$app->response->setStatus(200);
+		$response['success'] = true;
+		$response = json_encode($response, JSON_UNESCAPED_UNICODE);
+		$app->response->setBody($response);
 	});
 ?>
