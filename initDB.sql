12d131edbd9ef031f719f07332cadb6da358a6f7
Stavros Tsourlidakis <s.tsourlidakis@hotmail.com>
2016-10-01 15:42:28 +0300

    create table raw_scores

---
+++ initDB.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `raw_scores` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `username` varchar(40) NOT NULL,
+ `year` int(11) NOT NULL,
+ `month` int(11) NOT NULL,
+ `gen_score` int(11) NOT NULL,
+ `placeholder1` int(11) NOT NULL,
+ `placeholder2` int(11) NOT NULL,
+ `placeholder3` int(11) NOT NULL,
+ PRIMARY KEY (`id`)
+)
